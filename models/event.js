891fc1ca9c5edc77907ab88f1c907386c10d1c78
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2023-11-28 00:02:54 +0100

    Dodati event model i organizer model

---
+++ models/event.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema,model ,models } from "mongoose";
+
+const eventSchema = new Schema({
+    id:{
+        type: Number,
+        unique: [true, 'ID already in use!'],
+        required: [true,'ID is required!']
+    },
+    name: {
+        type : String,
+        required : [true, 'Name is required'],
+        match: [/^[a-zA-Z0-9]{1,30}$/, "Name can contain up to 30 alphanumeric letters!"]
+    },
+    starts_at: {
+        type: Date,
+        required: true
+    },
+    ends_at: {
+        type: Date,
+        required: true,
+        validate:{
+            validator: function (ends_at){
+                return ends_at > this.starts_at;
+            },
+            message: "End date must be greater than start date!",
+        }
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    users_signed: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "User",
+        default: [],
+        validate: {
+            validator: function(users_signed){
+                return users_signed.length < this.capacity;
+            },
+            message: "Event is full!",
+        }
+    },
+    image: {
+        type: [String]
+    }
+})
+
+const Event = models.Event || model("Event", eventSchema)
+
+export default Event;
