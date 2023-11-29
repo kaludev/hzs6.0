f58a1ff066fcb18f34d6d48cef6b543f02ae2090
Kaludev <luka.markovic2017@gmail.com>
2023-11-29 16:24:05 +0100

    added requestsPage

--- components/Requests/Requests.jsx
+++ components/Requests/Requests.jsx
@@ -3,43 +3,72 @@ import { useState,useEffect } from 'react'
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const PromptCardList = ({data,handleTagClick}) =>{
+const RequestCardList = ({data,handleConfirm,handleDecline}) =>{
   return (
     <div className="mt-16 prompt_layout">
-      {data.map(post =>(
-        <RequestCard key={post._id}
-        post = {post}
-        handleTagClick={handleTagClick} />
+      {data.map(request =>(
+        <RequestCard key={request._id}
+        data = {request}
+        handleConfirm={() => handleConfirm(request._id)}
+        handleDecline={() => handleDecline(request._id)}
+         />
       ))}
     </div>
   )
 }
-const Requests = (session,isSuperAdmin) => {
-  const [Posts, setPosts] = useState([]);
-
-  useEffect( () =>{
-    const fetchPosts = async () => {
+const Requests = (session) => {
+    const [Posts, setPosts] = useState([]);
+    const handleConfirm= async (id) => {
         try{
-            const res = await fetch('/api/organizator/getrequests');
+            const res = await fetch(`/api/organizator/confirmreq/${id}`);
             if(!res.ok){
                 new Error(res.text);
+            }else{
+                toast.success("Uspesno prihvacen zahtev");
             }
-            const data = await res.json()
-            setPosts(data);
         }catch(e){
             toast.error(e.message);
         }
     };
-    fetchPosts();
+    const handleDelete= async (id) => {
+        try{
+            const res = await fetch(`/api/organizator/declinereq/${id}`);
+            if(!res.ok){
+                new Error(res.text);
+            }else{
+                toast.success("Uspesno odbijen zahtev");
+            }
+        }catch(e){
+            toast.error(e.message);
+        }
+    };
+    useEffect( () =>{
+        const fetchPosts = async () => {
+            if(session?.user.isSuperAdmin) {
+                try{
+                    const res = await fetch('/api/organizator/getrequests');
+                    if(!res.ok){
+                        new Error(res.text);
+                    }
+                    const data = await res.json()
+                    setPosts(data);
+                }catch(e){
+                    toast.error(e.message);
+                }
+            }
+        };
+        fetchPosts();
 
-  },[session])
-  return (
-    <section className='Requests' >
-      <RequestCardList
-        data = {Posts}
-        />
-    </section>
-  )
+    },[session])
+    return (
+        <section className='Requests' >
+        <RequestCardList
+            data = {Posts}
+            handleConfirm={handleConfirm}
+            handleDecline = {handleDelete}
+            />
+        </section>
+    )
 
 }
 
