001bf44b1f1f00cd96c7b088394b4b43e2a29f7f
nickola23 <nikolamilanovic2305@gmail.com>
2023-11-30 18:58:21 +0100

    reseni bugovi

---
+++ components/EventsAdmin/EventsAdmin.jsx
@@ -0,0 +1,32 @@
+import EventCard from "@components/EventCard/EventCard"
+import { useSession } from "next-auth/react"
+import styles from './EventsAdmin.module.css'
+
+const Events = ({data,handleSubmit,handleLeave,handleEdit,handleDelete,providers,signIn}) => {
+  return (
+    <section className={styles.cardsMainSection}>
+          <section className={styles.cardsSection}>
+            {
+            data && data.length > 0 ? (
+              data.map(event => (
+                <EventCard
+                  key={event._id}
+                  eventName ={event.name} eventDesc={event.description} 
+                  address={event.address} eventStartTime={new Date(event.starts_at)}
+                  eventEndTime={new Date(event.ends_at)} applied={event.users_signed.length} 
+                  maxCapacity = {event.capacity} likes={event.likes}
+                  handleSubmit={handleSubmit && (() => handleSubmit(event._id))} providers={providers}
+                  handleLeave={() => handleLeave(event._id)}
+                  signIn={signIn} users_signed={event.users_signed}
+                  handleEdit={handleEdit && (() => handleEdit(event._id))} handleDelete={handleDelete && (() => handleDelete(event._id))}
+                />
+            ))) : (
+              <div className={emptyStyles.emptyArr}><span className={emptyStyles.colored}>Još uvek se niste prijavili za takmičenja.</span><br />Kako biste videli svoja takmičenja, pogledajte arenu i prijavite se</div>
+            )
+            }
+          </section>
+      </section>
+  )
+}
+
+export default Events
